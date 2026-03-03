0bf135d86761bd0c7a54922f1e0bef5277da4dc0
MrAlvaroLG <alvarolg.developer@gmail.com>
2026-03-02 22:33:05 -0500

    feat: agregar Vercel Analytics y Speed Insights para mejorar el rendimiento y análisis

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,8 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
+import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+      
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Analytics />
+        <SpeedInsights />
         {children}
       </body>
     </html>
